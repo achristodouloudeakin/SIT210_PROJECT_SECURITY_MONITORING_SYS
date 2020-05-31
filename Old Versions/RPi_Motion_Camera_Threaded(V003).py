6ebea57a306470891ca96b209919cb7b35a12c5e
achristodouloudeakin <62731909+achristodouloudeakin@users.noreply.github.com>
2020-05-31 15:30:29 +1000

    Create RPi_Motion_Camera_Threaded(V003).py

---
+++ Old Versions/RPi_Motion_Camera_Threaded(V003).py
@@ -0,0 +1,124 @@
+import RPi.GPIO as GPIO
+import paho.mqtt.client as mqtt
+
+from picamera import PiCamera
+from datetime import datetime
+
+import time
+import sys
+import requests
+import _thread
+
+primary_client = mqtt.Client("makerio_mqtt")
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+
+camera_sys = PiCamera()
+
+
+def argon_notification_procedure():
+    timer_x = 0
+    while timer_x < 10:
+        secondary_client = mqtt.Client("makeriio_mqtt")
+        secondary_client.connect("test.mosquitto.org", 1883)
+        secondary_client.subscribe("argon_Log_AC_3")
+        secondary_client.publish("RPi_AC", "Active")
+        print("Sending data to ARGON; Active; time:" + str(timer_x))
+        time.sleep(1)
+        timer_x += 1
+
+    secondary_client = mqtt.Client("makeriio_mqtt")
+    secondary_client.connect("test.mosquitto.org", 1883)
+    secondary_client.subscribe("argon_Log_AC_3")
+    secondary_client.publish("RPi_AC", "Non_Active")
+    print("Sending data to ARGON; Non_ACTIVE")
+
+    secondary_client.unsubscribe("argon_Log_AC_3")
+    
+    _thread.exit()
+#     except:
+#         print("Unable to Exit Thread for Argon 2 Connection")
+#     
+
+
+def camera_procedure():
+    print("Thread Starting for Camera")
+    print("Camera Starting")
+
+    #Rotate Camera Imaging
+    camera_sys.rotation = 180
+
+    file_capture = "Entranceway Recording at " + \
+        time.strftime("%Y-%m-%d---%H-%M-%S") + ".h264"
+
+    camera_sys.start_recording(file_capture)
+    time.sleep(7)
+    camera_sys.stop_recording()
+
+    print("Video Recorded")
+    time.sleep(2)
+
+    print("Thread Exiting for Camera")
+    _thread.exit()
+    
+#     try:
+#         print("Thread Exiting for Camera")
+#         _thread.exit()
+#         
+#     except:
+#         print("Unable to Exit Thread for Camera")
+    
+
+
+
+def message_function(client, userdata, message):
+    topic = str(message.topic)
+    message = str(message.payload.decode("utf-8"))
+    message.replace("argon_Log_AC_TEST", '')
+
+    range_in_centimetres = int(message)
+    
+    print(range_in_centimetres)
+
+    if (range_in_centimetres < 100):
+        print("Someone has openned the door")
+
+        # print("Stopping MQTT Connection Loop")
+        client.loop_stop()
+
+        # camera_procedure()
+
+        _thread.start_new_thread(camera_procedure, ())
+        _thread.start_new_thread(argon_notification_procedure, ())
+        #_thread.start_new_thread(main, ())
+
+        print("Notifying User of Front Door Movement")
+        requests.post("https://maker.ifttt.com/trigger/ARGON_AC_TEST_DETECTED_MOVEMENT/with/key/eU_JJJKmZOp_tczeQ56DCVRWKFmnvYPiAZ1fMz0oI6U")
+
+        #print("Procedure Exited, Restarting MQTT Connection Loop")
+        #time.sleep(4)
+        main()
+
+    else:
+        print("Doorway clear")
+
+
+def main():
+    try:
+        # Primary Client Between Argon_AC_TEST and RPi
+        primary_client = mqtt.Client("makerio_mqtt")
+        primary_client.connect("test.mosquitto.org", 1883)
+        primary_client.subscribe("argon_Log_AC_TEST")
+        primary_client.on_message = message_function
+        primary_client.loop_start()
+
+        while(1):
+            time.sleep(1)
+
+    except:
+        KeyboardInterrupt()
+
+
+main()
